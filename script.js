8ae36cc7299315192f421b2094b0cf7ecb14005d
huntergleach <hunter.leach21@gmail.com>
2021-05-31 14:48:56 -0500

    first iteration of personal website completed.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
-h2.textContent = "do not forget your click events, this is javascript speaking"; 
+h2.textContent = "Creatively minded, collaboratively focused. Let's make something together!"; 
 
 document.querySelector('body').appendChild(h2); 
